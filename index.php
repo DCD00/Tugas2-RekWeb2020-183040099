4947d21c252c13c6fc26d64b0090eb9dbfb55b30
Drajat Cahya Diningrat <71449473+DCD00@users.noreply.github.com>
2020-09-20 10:53:47 +0700

    file index.php
    file index ini untuk bisa mengakses dari latihan 5, 6, 7, dan tugas 3 praktikum pw

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Index</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/halamanindex.css">
+</head>
+<body>
+
+	   <!-- Jumbotron -->
+      <div class="jumbotron text-center">
+          <div class="neon">HALAMAN INDEX</div>
+      </div>
+    <!-- Akhir Jumbotron -->
+
+    <!-- Daftar Link -->
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <br>
+            <br>
+            <h3>DAFTAR LINK</h3>
+            <P>Berikut dibawah ini link untuk menuju ke latihan 5 a, b, c, dan d. Untuk menuju latihan yang anda pilih silahkan klik gambar dibawah ini.</P>
+            <br>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-1 col-sm-offset-3">
+            <a <?php echo '<a href=modul5/Latihan5a.php'; ?> class="thumbnail">
+              <img src="Image/aa.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul5/Latihan5b.php'; ?> class="thumbnail">
+              <img src="Image/bb.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul5/Latihan5c.php'; ?> class="thumbnail">
+              <img src="Image/cc.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul5/Latihan5d.php'; ?> class="thumbnail">
+              <img src="Image/dd.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul5/Latihan5e/Login.php'; ?> class="thumbnail">
+              <img src="Image/e.png">
+            </a>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <P>Berikut dibawah ini link untuk menuju ke latihan o dan latihan 6 a, b, c, d. Untuk menuju latihan yang anda pilih silahkan klik gambar dibawah ini.</P>
+            <br>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-1 col-sm-offset-3">
+            <a <?php echo '<a href=modul6/latihano/index.php'; ?> class="thumbnail">
+              <img src="Image/o.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul6/latihan6a/index.php'; ?> class="thumbnail">
+              <img src="Image/aa.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul6/latihan6b/index.php'; ?> class="thumbnail">
+              <img src="Image/bb.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul6/latihan6c/index.php'; ?> class="thumbnail">
+              <img src="Image/cc.png">
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+          <div class="col-sm-12 text-center">
+            <P>Berikut dibawah ini link untuk menuju ke latihan 7 a, b, c, d, dan Tugas3. Untuk menuju latihan yang anda pilih silahkan klik gambar dibawah ini.</P>
+            <br>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-1 col-sm-offset-3">
+            <a <?php echo '<a href=modul7/latihan7a/index.php'; ?> class="thumbnail">
+              <img src="Image/aa.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul7/latihan7b/index.php'; ?> class="thumbnail">
+              <img src="Image/bb.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul7/latihan7c/index.php'; ?> class="thumbnail">
+              <img src="Image/cc.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul7/latihan7d/index.php'; ?> class="thumbnail">
+              <img src="Image/dd.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=modul7/latihan7e/index.php'; ?> class="thumbnail">
+              <img src="Image/e.png">
+            </a>
+          </div>
+          <div class="col-sm-1">
+            <a <?php echo '<a href=Tugas3/login.php'; ?> class="thumbnail">
+              <img src="Image/T.png">
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir Daftar Link -->
+
+    <!-- Footer -->
+    <footer>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-sm-12">
+            <h3><marquee =alternate><a href="http://instagram.com/cahyarat">DRAJAT CAHYA DININGRAT</a></marquee =alternate></h3>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- Akhir Footer -->
+
+     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
